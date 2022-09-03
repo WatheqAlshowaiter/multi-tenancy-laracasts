814ee6506cfc23b836af4a8104bea6ea1b1f2e0c
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2022-09-03 21:12:28 +0300

    comment to make sure

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -20,7 +20,7 @@ class Phone extends Model
         static::addGlobalScope(new TenantScope);
 
         static::creating(function ($model) {
-            if (session()->has('tenant_id')) {
+            if (session()->has('tenant_id')) { //
                 $model->tenant_id = session()->get('tenant_id');
             }
         });
